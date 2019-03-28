ad7426ddb0374b56336990af197a5aef21ccba2a
Macias Blanco <maciasjblanco@gmail.com>
2019-03-28 05:55:45 -0400

    sala reporte general2

--- modules/sala/views/salareportegeneralss/index.php
+++ modules/sala/views/salareportegeneralss/index.php
@@ -2,6 +2,7 @@
 use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\bootstrap\Modal;
+use kartik\export\ExportMenu;
 use kartik\grid\GridView;
 use johnitvn\ajaxcrud\CrudAsset; 
 use johnitvn\ajaxcrud\BulkButtonWidget;
@@ -10,50 +11,67 @@ use johnitvn\ajaxcrud\BulkButtonWidget;
 /* @var $searchModel app\modules\sala\models\SalareportegeneralssSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Salareportegeneralsses';
+$this->title = 'Reporte General';
+$this->params['breadcrumbs'][] = ['label' => 'Establecimiento', 'url' => ['/sala/establecimiento']];
 $this->params['breadcrumbs'][] = $this->title;
 
 CrudAsset::register($this);
 
 ?>
+
 <div class="salareportegeneralss-index">
     <div id="ajaxCrudDatatable">
-        <?=GridView::widget([
+    <?php
+    echo ExportMenu::widget([
+    'dataProvider' => $dataProvider,
+    'columns' =>require(__DIR__.'/_columns.php'),a
+    'exportConfig' => [
+        ExportMenu::FORMAT_EXCEL => true,
+        ExportMenu::FORMAT_TEXT => false,
+        ExportMenu::FORMAT_HTML => false,
+        ExportMenu::FORMAT_PDF => false,
+        ExportMenu::FORMAT_CSV => false,s
+    ],
+    'dropdownOptions' => [
+        'label' => 'Export All',
+        'class' => 'btn btn-secondary'
+    ]
+    ]) . "<hr>\n". GridView::widget([
             'id'=>'crud-datatable',
             'dataProvider' => $dataProvider,
             'filterModel' => $searchModel,
             'pjax'=>true,
             'columns' => require(__DIR__.'/_columns.php'),
-            'toolbar'=> [
-                ['content'=>
-                    Html::a('<i class="glyphicon glyphicon-plus"></i>', ['create'],
-                    ['role'=>'modal-remote','title'=> 'Create new Salareportegeneralsses','class'=>'btn btn-default']).
-                    Html::a('<i class="glyphicon glyphicon-repeat"></i>', [''],
-                    ['data-pjax'=>1, 'class'=>'btn btn-default', 'title'=>'Reset Grid']).
-                    '{toggleData}'.
-                    '{export}'
-                ],
-            ],          
+            //'toolbar'=> [
+            //    ['content'=>
+                    //Html::a('<i class="glyphicon glyphicon-plus"></i>', ['create'],
+                    //['role'=>'modal-remote','title'=> 'Create new Salareportegeneralsses','class'=>'btn btn-default']).
+             //       Html::a('<i class="glyphicon glyphicon-repeat"></i>', [''],
+             //       ['data-pjax'=>1, 'class'=>'btn btn-default', 'title'=>'Reset Grid']).
+             //       '{toggleData}'//.
+                    //'{export}'
+             //   ],
+            //],          
             'striped' => true,
             'condensed' => true,
             'responsive' => true,          
             'panel' => [
                 'type' => 'primary', 
-                'heading' => '<i class="glyphicon glyphicon-list"></i> Salareportegeneralsses listing',
-                'before'=>'<em>* Resize table columns just like a spreadsheet by dragging the column edges.</em>',
-                'after'=>BulkButtonWidget::widget([
-                            'buttons'=>Html::a('<i class="glyphicon glyphicon-trash"></i>&nbsp; Delete All',
-                                ["bulk-delete"] ,
-                                [
-                                    "class"=>"btn btn-danger btn-xs",
-                                    'role'=>'modal-remote-bulk',
-                                    'data-confirm'=>false, 'data-method'=>false,// for overide yii data api
-                                    'data-request-method'=>'post',
-                                    'data-confirm-title'=>'Are you sure?',
-                                    'data-confirm-message'=>'Are you sure want to delete this item'
-                                ]),
-                        ]).                        
-                        '<div class="clearfix"></div>',
+                'heading' => '<i class="glyphicon glyphicon-list"></i> Listado de Reporte General',
+                //'before'=>'<em>* Resize table columns just like a spreadsheet by dragging the column edges.</em>',
+                //'after'=>BulkButtonWidget::widget([
+                //            'buttons'=>Html::a('<i class="glyphicon glyphicon-trash"></i>&nbsp; Delete All',
+                //                ["bulk-delete"] ,
+                 //               [
+               //                     "class"=>"btn btn-danger btn-xs",
+                //                    'role'=>'modal-remote-bulk',
+                //                    'data-confirm'=>false, 'data-method'=>false,// for overide yii data api
+                //                    'data-request-method'=>'post',
+                //                    'data-confirm-title'=>'Are you sure?',
+                //                    'data-confirm-message'=>'Are you sure want to delete this item'
+                //                ]),
+                //        ]).                        
+                //  '<div class="clearfix"></div>',
             ]
         ])?>
     </div>
